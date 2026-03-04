6242a4fe72b3ee183b98733e424f469aab1478b5
Samad <Samad.o@outlook.com>
2026-03-03 20:21:11 -0500

    added difficulty selection

--- features/randText.js
+++ features/randText.js
@@ -7,8 +7,25 @@ fetch("./typing-speed-test-main/data.json")
     const medium = data.medium;
     const hard = data.hard;
 
-    let difficultyText = easy[Math.floor(Math.random() * easy.length)];
+    function defaultSetting() {
+      let difficultyText = easy[Math.floor(Math.random() * easy.length)];
+      textBox.innerText = difficultyText.text;
+    }
 
-    textBox.innerText = difficultyText.text;
+    document.getElementById("btn").addEventListener("click", (e) => {
+      if (!e.target.matches("button")) return; // Check which difficulty was selected
+
+      if (e.target.id === "easy") {
+        difficultyText = easy[Math.floor(Math.random() * easy.length)];
+      } else if (e.target.id === "medium") {
+        difficultyText = medium[Math.floor(Math.random() * medium.length)];
+      } else {
+        difficultyText = hard[Math.floor(Math.random() * hard.length)];
+      }
+
+      textBox.innerText = difficultyText.text;
+    });
+
+    defaultSetting();
   })
   .catch((error) => console.error("Error:", error));
